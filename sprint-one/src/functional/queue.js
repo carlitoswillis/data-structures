181a5c67ef5a8772bf521b3a90da3e5533c4eb92
Austin <asmit2222@gmail.com>
2020-03-25 19:01:04 -0400

    finished queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,31 @@
-var Queue = function() {
+var Queue = function () {
   var someInstance = {};
+  someInstance.counter = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
+  someInstance.storage = storage;
 
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {
+  someInstance.enqueue = function (value) {
+
+    someInstance.storage[someInstance.counter] = value;
+    someInstance.counter++;
+
   };
 
-  someInstance.dequeue = function() {
+  someInstance.dequeue = function () {
+    var key = Object.keys(storage)[0];
+    var dequeued = storage[key];
+    delete storage[key];
+    return dequeued;
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    return Object.keys(someInstance.storage).length;
   };
 
   return someInstance;
 };
+
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,20 @@
-var Stack = function() {
+var Stack = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
+  someInstance.counter = 0;
+  someInstance.storage = storage;
+
   // Implement the methods below
-  someInstance.push = function(value) {
+  someInstance.push = function (value) {
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function () {
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
   };
 
   return someInstance;
