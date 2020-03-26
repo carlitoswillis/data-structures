3a43f2679c397a81cf7b946f2dc10454307bfa90
carlitoswillis <carlitoswillis@berkeley.edu>
2020-03-26 16:29:27 -0700

    Implement linkedList
    doubly linked list with a few functions

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,39 +4,61 @@ var LinkedList = function () {
   list.tail = null;
   list.size = 0;
 
-
-  // addToTail() method, takes a value and adds it to the end of the list
   list.addToTail = function (value) {
 
     var newNode = Node(value);
-
-
-    // check if head is null
-    // check if tail is null
 
     if (list.head === null && list.tail === null) {
       list.head = newNode;
       list.tail = newNode;
     } else {
       list.tail.next = newNode;
+      newNode.prev = list.tail;
       list.tail = newNode;
     }
     list.size++;
   };
-  // .removeHead() method, removes the first node from the list and returns its value
+
   list.removeHead = function () {
     if (list.head === null) {
       return undefined;
     }
+
+    if (list.size > 0 ) {
+      list.size--;
+    }
+
     var value = list.head.value;
-    var second = list.head.next;
-    list.head = second;
+    if (list.size > 1) {
+      list.head.next.prev = null;
+    }
+    list.head = list.head.next;
     return value;
   };
 
   list.contains = function (target) {
-  };
 
+    var find = function (currnode, val) {
+      if (currnode === null) {
+        return false;
+      }
+      return (currnode.value === val) || find(currnode.next, val);
+    };
+
+    return find(list.head, target);
+
+  //old code:
+    // var result = false;
+    // var currNode = list.head;
+    // for (var i = 0; i < list.size; i++) {
+    //   if (currNode.value === target) {
+    //     return true;
+    //   } else {
+    //     currNode = currNode.next;
+    //   }
+    // }
+    // return result;
+  };
   return list;
 };
 
@@ -45,7 +67,7 @@ var Node = function (value) {
 
   node.value = value;
   node.next = null;
-  // node.prev = null;
+  node.prev = null;
 
   return node;
 };
