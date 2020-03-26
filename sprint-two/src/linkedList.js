832891b09d34c1b71fb363ffbe231a613a196835
Austin <asmit2222@gmail.com>
2020-03-26 14:13:53 -0400

    working on linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,25 +1,44 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.size = 0;
 
-  list.addToTail = function(value) {
+
+  // addToTail() method, takes a value and adds it to the end of the list
+  list.addToTail = function (value) {
+
+    var newNode = Node(value);
+
+
+    // check if head is null
+    // check if tail is null
+
+    if (list.head === null && list.tail === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+    list.size++;
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
   };
 
-  list.contains = function(target) {
+  list.contains = function (target) {
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
   node.next = null;
+  node.prev = null;
 
   return node;
 };
